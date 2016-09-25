eb1cc54a23efdcc1c0197516d92aab4cc53a1515
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-09-25 14:29:55 +0530

    fixed cosmetics with Direct power method

--- DirectPower.py
+++ DirectPower.py
@@ -1,19 +1,32 @@
 import numpy as np
+from math import copysign
 
-def DirectPower(mat,iterations):
+def DirectPower(mat,iterations,threshold):
     n = mat.shape[0]
     x = np.array([1 for i in range(n)])
     x.shape = (3,1)
     for i in range(iterations):
         u = mat.dot(x)
-        M = u.max()
-        x = u/M
+        try:
+            # print 1, u.max(), M
+            err = 100.0*(u.max()-M)/u.max()
+            err = copysign(err,1)
+            if err<threshold :
+                return u.max()
+            print i+1, u/u.max(), u.max(), err
+            
+            M = u.max()
+            x = u/M
+        except:
+            M = u.max()
+            x = u/M
+            print i+1, x, M
     return M
 if __name__=="__main__" :
     with open("input.txt") as f:
         mat = [[float(x) for x in line.split()] for line in f]
     mat = np.matrix(mat)
-    poo = DirectPower(mat,20)
+    poo = DirectPower(mat,20,0.1)
     print poo
 
 
