569433152fb6da94b98fcf02e52b1485ab887436
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-09-25 14:35:58 +0530

    More Cosmetic Fixes

--- DirectPower.py
+++ DirectPower.py
@@ -4,7 +4,7 @@ from math import copysign
 def DirectPower(mat,iterations,threshold):
     n = mat.shape[0]
     x = np.array([1 for i in range(n)])
-    x.shape = (3,1)
+    x.shape = (n,1)
     for i in range(iterations):
         u = mat.dot(x)
         try:
@@ -12,7 +12,7 @@ def DirectPower(mat,iterations,threshold):
             err = 100.0*(u.max()-M)/u.max()
             err = copysign(err,1)
             if err<threshold :
-                return u.max()
+                return u.max(),u/u.max()
             print i+1, u/u.max(), u.max(), err
             
             M = u.max()
@@ -21,12 +21,15 @@ def DirectPower(mat,iterations,threshold):
             M = u.max()
             x = u/M
             print i+1, x, M
-    return M
+    return M,x
+
 if __name__=="__main__" :
     with open("input.txt") as f:
         mat = [[float(x) for x in line.split()] for line in f]
     mat = np.matrix(mat)
-    poo = DirectPower(mat,20,0.1)
-    print poo
-
+    e,v = DirectPower(mat,20,0.1)
+    v.shape = (1,v.shape[0])
+    print "_"*64,"\n"
+    print "Eigen Value:\t",e
+    print "Eigen Vector:\t",v
 
