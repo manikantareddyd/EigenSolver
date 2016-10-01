43201e352aaf0f28fab1fb0426420c1b5341ca15
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-10-01 15:50:17 +0530

    Done with Cleaning

--- main.py
+++ main.py
@@ -18,13 +18,19 @@ for i in methodNameMap:
     print i,":",methodNameMap[i]
 
 method = int(input())
+import sys
+sys.stdout = open("out","w")
 if method == 1:
+    print method, ":", methodNameMap[method],"\n"
     import DirectPower
 elif method == 2:
+    print method, ":", methodNameMap[method],"\n"
     import InversePower
 elif method == 3:
+    print method, ":", methodNameMap[method],"\n"
     import ShiftedPower
 elif method == 4:
+    print method, ":", methodNameMap[method],"\n"
     import QR
 else:
     print "Wrong Choice"
